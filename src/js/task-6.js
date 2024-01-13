53bcea990e35fd67cfa27225ac6b6bdda5fe4af5
OlhaNazarova <108295915+OlhaNazarova@users.noreply.github.com>
2024-01-13 19:07:39 +0200

    all tasks

--- src/js/task-6.js
+++ src/js/task-6.js
@@ -1,3 +1,37 @@
+const refs = {
+  div: document.querySelector("#controls"),
+  input: document.querySelector("input"),
+  divBoxes: document.querySelector("#boxes"),
+};
+
+refs.div.addEventListener("click", onClickDivConteiner);
+
+function onClickDivConteiner(evt) {
+  const button = evt.target.textContent;
+  if (button === "Create") {
+    const userInput = Number(refs.input.value);
+    createBoxes(userInput);
+  } else if (button === "Destroy") {
+    reset();
+  }
+}
+
+function createBoxes(amount) {
+  const elemtsDiv = [];
+  for (let i = 0; i < amount; i += 1) {
+    const elem = document.createElement("div");
+    elem.style.width = `${30 + i * 10}px`;
+    elem.style.height = `${30 + i * 10}px`;
+    elem.style.backgroundColor = getRandomHexColor();
+    elemtsDiv.push(elem);
+  }
+  refs.divBoxes.append(...elemtsDiv);
+}
+
+function reset() {
+  refs.divBoxes.innerHTML = "";
+  refs.input.value = "";
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)

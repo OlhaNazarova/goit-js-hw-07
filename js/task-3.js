f60cd943033eb59b6ed420b5230ad1c4e48deeb6
OlhaNazarova <108295915+OlhaNazarova@users.noreply.github.com>
2024-01-15 20:31:55 +0200

    task 3 and 6 corrected

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,7 @@
-const refs = {
-    inputEl: document.querySelector("#name-input"),
-    spanEl: document.querySelector("#name-output"),
-  };
-  
-  refs.inputEl.addEventListener("input", onInputEl);
-  
-  function onInputEl(event) {
-    refs.spanEl.textContent = event.currentTarget.value;
-    if (!refs.spanEl.textContent) {
-      refs.spanEl.textContent = "Anonymous";
-    }
-  }
+const nameInput = document.querySelector("input#name-input");
+const nameOuput = document.querySelector("span#name-output");
+
+nameInput.addEventListener("input", (event) => {
+  nameOuput.textContent = event.currentTarget.value.trim() || "Anonymous";
+});
   
--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,41 @@
-const refs = {
-  amountEl: document.querySelector('#controls input'),
-  buttonCreateEl: document.querySelector('button[data-create]'),
-  buttonDesrtoyEl: document.querySelector('button[data-destroy]'),
-  addEl: document.querySelector('#boxes'),
-};
-
-refs.amountEl.addEventListener('blur', event => {
-  let amount = event.currentTarget.value;
-  refs.buttonCreateEl.addEventListener('click', () => {
-    refs.amountEl.value = '';
-    createBoxes(amount);
-    amount = '';
-  });
-});
-
-refs.buttonDesrtoyEl.addEventListener('click', () => {
-  refs.addEl.innerHTML = '';
-  size = 20;
-});
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-let size = 20;
+const numberInput = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("div#boxes");
+
 function createBoxes(amount) {
-  for (let i = 0; i < amount; i += 1) {
-    size += 10;
-    const divboxEl = document.createElement('div');
-    divboxEl.style.width = `${size}px`;
-    divboxEl.style.height = `${size}px`;
-    divboxEl.style.backgroundColor = getRandomHexColor();
-    refs.addEl.append(divboxEl);
+  destroyBoxes();
+  let markup = "";
+
+  for (let i = 0, j = 30; i < amount; i += 1, j += 10) {
+    markup += `
+    <div style="background: ${getRandomHexColor()};
+    width:${j}px;height:${j}px;">
+    </div>
+    `;
   }
+  boxes.insertAdjacentHTML("afterbegin", markup);
+  boxes.style.display = "flex";
+  boxes.style.gap = "16px";
+  boxes.style.flexWrap = "wrap";
 }
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const numOfBoxes = numberInput.value;
+
+  if (numOfBoxes < 1 || numOfBoxes > 100) return;
+  createBoxes(numOfBoxes);
+  numberInput.value = "";
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
